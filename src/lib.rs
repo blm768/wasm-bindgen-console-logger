dc7bf83bb4242fcf34c2d4ba5d667eba1f2f9d11
Ben Merritt <blm768@gmail.com>
2019-01-23 22:23:04 -0800

    Make log levels configurable directly in ConsoleLogger

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,7 @@ use wasm_bindgen::prelude::*;
 
 pub const DEFAULT_LOGGER: ConsoleLogger = ConsoleLogger {
     formatter: &format_message,
+    log_level: Level::Trace,
 };
 
 fn format_message(record: &Record) -> String {
@@ -46,14 +47,22 @@ pub type RecordFormatter = Fn(&Record) -> String + Send + Sync;
 /// All other messages will be logged with `console.log()`.
 pub struct ConsoleLogger {
     formatter: &'static RecordFormatter,
+    log_level: Level,
 }
 
 impl ConsoleLogger {
     /// Constructs a new `ConsoleLogger`
     ///
     /// The given function will be used to format the logged messages.
-    pub fn new(formatter: &'static RecordFormatter) -> Self {
-        ConsoleLogger { formatter }
+    pub fn new(formatter: &'static RecordFormatter, log_level: Level) -> Self {
+        ConsoleLogger { formatter, log_level }
+    }
+
+    /// Constructs a `ConsoleLogger` with the given maximum log level
+    /// 
+    /// A default log formatter will be used.
+    pub fn with_level(level: Level) -> Self {
+        ConsoleLogger::new(&format_message, level)
     }
 }
 
@@ -64,8 +73,8 @@ impl Default for ConsoleLogger {
 }
 
 impl Log for ConsoleLogger {
-    fn enabled(&self, _: &Metadata) -> bool {
-        true
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= self.log_level
     }
 
     fn log(&self, record: &Record) {
